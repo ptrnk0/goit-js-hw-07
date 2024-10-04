045448fc87d0d6a1e1770ac4df48d8a8838a2c6f
ptrnk0 <140536679+ptrnk0@users.noreply.github.com>
2024-10-04 08:47:09 +0000

    Add function in task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,41 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+	let divSize = 20;
+	const divArr = [];
+
+	for (let index = 0; index <= amount; index++) {
+		const newDiv = document.createElement("div");
+		divSize += 10;
+
+		newDiv.style.width = `${divSize}px`;
+		newDiv.style.height = `${divSize}px`;
+		newDiv.style.backgroundColor = getRandomHexColor();
+
+		divArr.push(newDiv);
+	}
+
+	return divArr;
+}
+
+function destroyBoxes() {
+	document.querySelector("#boxes").innerHTML = "";
+}
+
+const inputDiv = document
+	.querySelector("input[type='number']")
+	.addEventListener("change", (event) => {
+		event.currentTarget.value;
+	});
+
+document
+	.querySelector("button[data-create]")
+	.addEventListener("click", (event) => {
+		document
+			.querySelector("#boxes")
+			.append(...createBoxes(Number(inputDiv)));
+	});
